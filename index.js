54de5d249d6d5d05bff0ea317b1d74a019c23079
Jeet Prajapati <jeetprajapati21@gmail.com>
2021-07-18 00:48:52 +0530

    make it better

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ app.use('/api/auth', authRouter);
 app.use('/api/note', noteRouter);
 app.use('/api/user', userRouter);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require('path');
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`Server running on port: 5000`);
 });
